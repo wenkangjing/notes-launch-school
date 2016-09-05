7b47ba50923f24f430b84cfc8f25aabbb72d4b42
Kangjing Wen <wenkangjing@gmail.com>
2016-09-05 21:35:31 +1000

    Done Ruby Variables

---
+++ 100/var_3_4_name.rb
@@ -0,0 +1,18 @@
+# print user name 10 times
+
+puts "What is your name?"
+name = gets.chomp
+
+10.times do 
+puts name
+end
+
+puts "waht is your first name?"
+fname = gets.chomp
+puts "what is your last name?"
+lname = gets.chomp
+
+puts "#{fname} #{lname}"
+
+
+
