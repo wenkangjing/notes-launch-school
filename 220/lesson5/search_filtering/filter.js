154debe0368d4a61a732a41a73ccb28dcf82fcca
Kangjing Wen <wenkangjing@gmail.com>
2017-01-18 17:30:54 +1100

    220 - Lesson 5 - 6/8 (search filtering)

--- 220/lesson5/search_filtering/filter.js
+++ 220/lesson5/search_filtering/filter.js
@@ -1,13 +1,62 @@
+var items = [{
+  "title": "The Legend of Zelda: Majora's Mask 3D",
+  "id": 1,
+  "category": "Nintendo 3DS"
+}, {
+  "title": "Super Smash Bros.",
+  "id": 2,
+  "category": "Nintendo 3DS"
+}, {
+  "title": "Super Smash Bros.",
+  "id": 3,
+  "category": "Nintendo WiiU"
+}, {
+  "title": "LEGO Batman 3: Beyond Gotham",
+  "id": 4,
+  "category": "Nintendo WiiU"
+}, {
+  "title": "LEGO Batman 3: Beyond Gotham",
+  "id": 5,
+  "category": "Xbox One"
+}, {
+  "title": "LEGO Batman 3: Beyond Gotham",
+  "id": 6,
+  "category": "PlayStation 4"
+}, {
+  "title": "Far Cry 4",
+  "id": 7,
+  "category": "PlayStation 4"
+}, {
+  "title": "Far Cry 4",
+  "id": 8,
+  "category": "Xbox One"
+}, {
+  "title": "Call of Duty: Advanced Warfare",
+  "id": 9,
+  "category": "PlayStation 4"
+}, {
+  "title": "Call of Duty: Advanced Warfare",
+  "id": 10,
+  "category": "Xbox One"
+}]
+
 $(function(){
   let $categories = $('input[type=checkbox]');
 
+  $categories.on('click', function(e) {
+    let $e = $(this);
 
- $categories.on('click', function(e) {
-   
-   let $e = $(this);
-   let category = $e.parent().text();
-   console.log($e.prop("checked"), );
-  
-    //$('article').hide().filter('[data-block=' + $(this).attr('data-block') + ']').show();
+    let ids = items.filter(function(item) { 
+      return item.category === $e.parent().text();
+    }).map(function(item) {
+      return item.id;
+    });
+
+    $("main li").each(function(index, element){
+      let id = parseInt($(this).attr('data-id'), 10);
+      if (ids.indexOf(id) !== -1) {
+          $(this).toggle($e.prop("checked"))
+      }
+    });
   });
 })
