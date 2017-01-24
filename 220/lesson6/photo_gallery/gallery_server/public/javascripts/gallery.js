8adc054b1aff4cff73544de5f4d54b817870f478
Kangjing Wen <wenkangjing@gmail.com>
2017-01-24 11:40:17 +1100

    220 - Lesson 6 - 9/11 (my proposal)

--- 220/lesson6/photo_gallery/gallery_server/public/javascripts/gallery.js
+++ 220/lesson6/photo_gallery/gallery_server/public/javascripts/gallery.js
@@ -1,19 +1,101 @@
 $(function() {
   // catch templates and data
   let templates = {},
-      photos = [];
+      photos = [],
+      current_idx = 0;
 
-  $('script[type="text/x-handlebars"]').each(function() {
-    var $tmpl = $(this);
-    templates[$tmpl.attr('id')] = Handlebars.compile($tmpl.html());
+  catchTempaltes();
+  reloadPhotos();
+
+  function catchTempaltes() {
+    // cache the compiled templates
+    $('script[type="text/x-handlebars"]').each(function() {
+      var $tmpl = $(this);
+      templates[$tmpl.attr('id')] = Handlebars.compile($tmpl.html());
+    });
+
+    // register partial
+    $("[data-type=partial]").each(function () {
+      var $partial = $(this);
+      Handlebars.registerPartial($partial.attr("id"), $partial.html());
+    });
+  }
+
+  function reloadPhotos() {
+    // get photos and render in success callback
+    $.ajax({
+      url: "/photos",
+      success: function(json) {
+        photos = json;
+        renderPhotos();
+        navigateTo(current_idx);
+      }
+    });
+  }
+
+  function renderPhotos() {
+    $("#slides").append(templates.photos({photos: photos}));
+  }
+
+  function showPhoto(idx) {
+    $("#slides").children().hide();
+    $("#slides").children().eq(idx).show();
+  }
+
+  function renderPhotoInformation(idx) {
+    $("section > header").children().remove();
+    $("section > header").append(templates.photo_information(photos[idx]));
+  }
+
+  // get comments by photo id and render in success callback
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: "/comments",
+      data: "photo_id=" + idx,
+      success: function(comment_json) {
+        $("#comments ul").children().remove();
+        $("#comments ul").append(templates.comments({comments: comment_json}));
+      }
+    });
+  }
+
+  function navigateTo(idx) {
+    showPhoto(idx);
+    renderPhotoInformation(idx)
+    getCommentsFor(photos[idx].id);
+  }
+
+
+  // User Interactions
+  $("a.next").on("click", function(e) {
+    e.preventDefault();
+    let idx = getNext();
+    navigateTo(idx);
   });
 
-  $.ajax({
-    url: "/photos",
-    success: function(json) {
-      photos = json;
-      console.log(photos);
+  $("a.prev").on("click", function(e) {
+    e.preventDefault();
+     let idx = getPrev();
+     navigateTo(idx);
+  });
+
+  function getNext() {
+    var length = photos.length;
+    if (current_idx === length - 1) {
+      current_idx = 0;
+    } else {
+      current_idx++;
     }
-  });
+    return current_idx;
+  }
 
+  function getPrev() {
+    var length = photos.length;
+    if (current_idx === 0) {
+      current_idx = length - 1;
+    } else {
+      current_idx--;
+    }
+    return current_idx;
+  }
 });
